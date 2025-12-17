55b48b43b9bbd799160ac85350c66d3b0b21d7d1
Wlady123 <dw.canart@uea.edu.ec>
2025-12-17 00:40:27 -0500

    Agregar clase Producto

--- tienda_virtual.py
+++ tienda_virtual.py
@@ -4,3 +4,8 @@ CASO 2: TIENDA VIRTUAL (MUNDO REAL)
 Este programa simula una tienda virtual básica
 utilizando Programación Orientada a Objetos (POO).
 """
+# Clase Producto: representa un producto de la tienda
+class Producto:
+    def __init__(self, nombre, precio):
+        self.nombre = nombre
+        self.precio = precio
