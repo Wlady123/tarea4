56d60fa80d0a2e196173569deff73a686b108957
Wlady123 <dw.canart@uea.edu.ec>
2025-12-16 23:30:01 -0500

    Agregar descripción del sistema de reservas

---
+++ reservas.py
@@ -0,0 +1,6 @@
+"""
+Sistema de Reservas de Hotel
+Descripción del caso del mundo real:
+Este programa simula un sistema básico de reservas de un hotel
+usando Programación Orientada a Objetos (POO).
+"""
